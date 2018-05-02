ba1341152ccdd5a3464cd6a9ae36c568bc41791a
kobi <kobi.b@inner-active.com>
2018-05-02 11:27:06 +0300

    top-level: all deps

--- src/topLevelDeps.ts
+++ src/topLevelDeps.ts
@@ -8,16 +8,29 @@ const prom = {
 }
 
 export async function topLevelDeps(packages: Package[]) {
+  const packageNameToPackage = new Map<string, Package>(
+    packages.map(p => [p.json.name, p] as any)
+  )
   let devDeps: any = packages.reduce((prev, current) => {
-    const sameKey = Object.keys(current.json.dependencies).find(key => prev.hasOwnProperty(key))
-    if(current.json.dependencies[sameKey] !== prev[sameKey]) {
-      console.warn(current.json.name, `same key ${sameKey}, different version ${current.json.dependencies[sameKey]}`)
+    const deps = {
+      ...current.json.dependencies,
+      ...current.json.devDependencies
     }
-    return {...prev, ...current.json.dependencies}
+
+    const sameKey = Object.keys(deps).find(key => prev.hasOwnProperty(key))
+    if(deps[sameKey] !== prev[sameKey]) {
+      console.warn(current.json.name, `same key ${sameKey}, different version ${deps[sameKey]}`)
+    }
+
+    const toAdd = Object.keys(deps)
+      .filter(key => !packageNameToPackage.has(key))
+      .reduce((prev, key) => ({...prev, [key]: deps[key]}), {})
+
+    return {...prev, ...toAdd}
   }, {} as Record<string, string>)
   devDeps = Object.keys(devDeps).sort().reduce((prev, next) => ({...prev, [next]: devDeps[next]}), {})
   const path = `${process.env.PWD}/package.json`
   const packageJson: PackageJSON = JSON.parse(await prom.readFile(path, {encoding: "utf8"}))
-  packageJson.dependencies = devDeps
+  packageJson.devDependencies = devDeps
   await prom.writeFile(path, JSON.stringify(packageJson, null, 2))
 }
